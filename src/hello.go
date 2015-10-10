4cad00829842609f0b36b65c9ff522eafa8b532f
Dave Hawes <dhawes667@googlemail.com>
2015-10-10 14:02:16 +0100

    Work on sudoku solver

--- src/hello.go
+++ src/hello.go
@@ -2,8 +2,160 @@ package main
 
 import (
 	"fmt"
-//	"strings"
 )
+
+//General solver error
+type SolveError struct{
+	m_info string
+}
+
+func (e SolveError) Error() string {
+	return e.m_info
+}
+
+//General solver interface
+type Solver interface {
+	Solved() (bool,SolveError)
+}
+
+//Cells which store individual numbers in the grid
+type cell struct {
+	m_possible map[int]bool 
+}
+
+func (c cell) SetKnownTo(value int){
+	for k,v := range c.m_possible{
+		if k != value {
+			delete(c.m_possible,k)
+		}
+	} 
+}
+
+func (c cell) TakeKnownFromPossible(known []int){
+	for _,v := range known{
+		delete(c.m_possible,v)
+	}
+}
+
+func (c cell) Known() (int, *SolveError){
+	
+	// by convention we delete from the map possibles that are no longer possible
+	// so we just need to check map length to see if the cell is solved
+	if len(c.m_possible)!=1{
+		return 0,&SolveError{"Value not yet known for this cell"}
+	}
+	
+	//Only one key is now considered "possible", it's value should be true, and it should
+	//be the only one in the list, return it if that is the case 
+	for k,v := range c.m_possible {
+		if v{
+			return k,nil
+		}
+	}
+	
+	return 0,&SolveError{"Error in cell storage of known values"}
+}
+
+func (cells []*cell) Solved() bool{
+	for _,c := range cells{
+		_,err := c.Known();
+		if err != nil{
+			return false
+		}
+	}
+}
+
+//Squares which represent one of each of the 9 squares in a grid, each of which 
+//references a 3x3 collection of cells.
+type square struct {
+	m_cells []*cell
+}
+
+func (s square) Solved() (bool,SolveError) {
+	return m_cells.Solved(),nil
+}
+
+//A horizontal or vertical line of 9 cells through the entire grid.
+type line struct {
+	m_cells []*cell
+}
+
+func (l line) Solved() (bool,SolveError) {
+	return m_cells.Solved()
+}
+
+//Grid which represents the 3x3 collection of squares which represent the entire puzzle
+const ROW_LENGTH = 9
+const COL_LENGTH = 9
+const NUM_SQUARES = COL_LENGTH
+
+type grid struct {
+	m_squares 	[]square
+	m_rows		[]line
+	m_cols		[]line
+	
+	m_sets		[]*solver
+	m_cells		[]cell
+}
+
+func New(puzzle [COL_LENGTH][ROW_LENGTH]int) (grid, SolveError){
+	var g grid
+	g.Init();
+	g.Fill(puzzle)
+	return g,nil
+} 
+
+func (g grid) Init() {
+	//Init the raw cells themselves that actually store the grid data
+	g.m_cells = make([]cell,COL_LENGTH*ROW_LENGTH)
+	
+	//Init each of the grouping structures that view portions of the grid
+	g.m_squares = make([]square,NUM_SQUARES)
+	g.m_rows = make([]line, ROW_LENGTH)
+	g.m_cols = make([]line,COL_LENGTH)
+	
+	//Make m_sets just a big long list of all the cell grouping structures
+	//handy for doing iterations over all different ways of looking at the cells
+	g.m_sets = make([]*solver,len(g.m_squares) + len(g.m_rows) + len(g.m_cols))
+	
+	var idx int
+	for _,s := range g.m_squares{
+		g.m_sets[idx++] = s 
+	}
+	
+	for _,r := range g.m_rows{
+		g.m_sets[idx++] = r 
+	}
+	
+	for _,c := range g.m_cols{
+		g.m_sets[idx++] = c 
+	}
+}
+
+func (g grid) Fill(puzzle [COL_LENGTH][ROW_LENGTH]int){
+	
+}
+
+func (g grid) Solved() bool {
+	for _,s := range g.m_sets{
+		solved,err = s.solved()
+		if err != nil{
+			fmt.Println("Error during Solved() check on grid: " + err.Error())
+			return false
+		}
+		
+		if !solved){
+			return false
+		}
+	}
+	
+	return true
+}
+
+type result struct {
+	url string
+	body string
+}
 
 type Fetcher interface {
 	// Fetch returns the body of URL and
@@ -42,11 +194,6 @@ func crawlSite(url string, depth int, fetcher Fetcher, ch chan result, resMap ma
 	}
 
 		
-}
-
-type result struct {
-	url string
-	body string
 }
 
 // Crawl uses fetcher to recursively crawl
