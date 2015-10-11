6dab628d043e159b6bb18f78f9e8d3f8bf598bb3
Dave Hawes <dhawes667@googlemail.com>
2015-10-11 16:20:37 +0100

    Main function written with puzze input, need to start testing input worked as expected

--- src/hello.go
+++ src/hello.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 //General solver error
@@ -11,6 +12,10 @@ type SolveError struct{
 
 func (e SolveError) Error() string {
 	return e.m_info
+}
+
+func (e SolveError) String() string {
+	return e.Error()
 }
 
 //General solver interface
@@ -54,6 +59,15 @@ func (c cell) Known() (int, *SolveError){
 	}
 	
 	return 0,&SolveError{"Error in cell storage of known values"}
+}
+
+func (c cell) String() string {
+	val,err := c.Known()
+	if(err != nil){
+		return err.Error()
+	}
+	
+	return strconv.Itoa(val)
 }
 
 type cellPtrSlice []*cell 
@@ -152,8 +166,9 @@ func (g grid) Init() {
 		g.m_squares[squareIdx].init()
 		for x :=0; x<SQUARE_SIZE; x++{
 			for y:= 0; y<SQUARE_SIZE; y++{
-				gridX := SQUARE_SIZE * (squareIdx % SQUARE_SIZE) + (x%SQUARE_SIZE)
-				gridY := SQUARE_SIZE * (squareIdx % SQUARE_SIZE) + (y%SQUARE_SIZE)
+				//is this correct?
+				gridX := SQUARE_SIZE * (squareIdx % SQUARE_SIZE) + x 
+				gridY := SQUARE_SIZE * (squareIdx / SQUARE_SIZE) + y
 				g.m_squares[squareIdx].m_cells[x][y] = &g.m_cells[gridX][gridY]
 			}
 		}
@@ -186,7 +201,16 @@ func (g grid) Init() {
 }
 
 func (g grid) Fill(puzzle [COL_LENGTH][ROW_LENGTH]int){
+	g.Init()
 	
+	for x:=0; x<COL_LENGTH; x++{
+		for y:=0; y<ROW_LENGTH; y++{
+			var puzzVal = puzzle[x][y]
+			if puzzVal >=1 && puzzVal<=9{
+				g.m_cells[x][y].SetKnownTo(puzzVal)
+			}
+		}
+	}
 }
 
 func (g grid) Solved() (bool,*SolveError) {
@@ -205,142 +229,37 @@ func (g grid) Solved() (bool,*SolveError) {
 	return true,nil
 }
 
-type result struct {
-	url string
-	body string
-}
-
-type Fetcher interface {
-	// Fetch returns the body of URL and
-	// a slice of URLs found on that page.
-	Fetch(url string) (body string, urls []string, err error)
-}
-
-func startCrawlSite(url string, depth int, fetcher Fetcher, ch chan result, resMap map[string]string){
-	defer close(ch)
-	crawlSite(url,depth,fetcher,ch, resMap)
-	
-}
-
-func crawlSite(url string, depth int, fetcher Fetcher, ch chan result, resMap map[string]string){
-	
-	if(depth <=0){
-		return
-	}
-	
-	if _,ok := resMap[url];ok {
-		return //already crawled this url
-	}
-	
-	ch<- result{url,""};
-	
-	body, urls, err := fetcher.Fetch(url)
-	if err != nil {
-		//fmt.Println(err)
-		return
-	}
-	
-	ch<- result{url,body};
-	
-	for _, u := range urls {
-		crawlSite(u, depth-1, fetcher, ch, resMap)
-	}
-
-		
-}
-
-// Crawl uses fetcher to recursively crawl
-// pages starting with url, to a maximum of depth.
-func Crawl(url string, depth int, fetcher Fetcher) {
-	
-	if depth <= 0 {
-		return
-	}	
-	
-	resultsMap := make(map[string]string) 
-	
-	body, urls, err := fetcher.Fetch(url)
-	if err != nil {
-		//fmt.Println(err)
-		return
-	}
-	
-	resultsMap[url] = body
-	resultChs := make([]chan result,0,depth)
-	
-	
-	for _, u := range urls {
-		siteResCh := make(chan result)
-		resultChs = append(resultChs,siteResCh)
-		
-		go startCrawlSite(u, depth-1, fetcher, siteResCh, resultsMap)
-	}
-	
-	for _,resCh :=  range resultChs {
-		
-			for v1 := range resCh {
-				resultsMap[v1.url] = v1.body
+func (g grid) String() string {
+	var str string
+	for x:=0; x < ROW_LENGTH; x++{
+		for y:=0; y < COL_LENGTH; y++{
+			cell := g.m_cells[y][x]
+			str += cell.String()
 		}
+		str+="\n"
 	}
-							   
 	
-	
-	for k,v := range resultsMap{
-		fmt.Println(k + " " + v)
-	}
-	return
+	return str
 }
+
+// Main code
 
 func main() {
-	Crawl("http://golang.org/", 4, fetcher)
-}
-
-// fakeFetcher is Fetcher that returns canned results.
-type fakeFetcher map[string]*fakeResult
-
-type fakeResult struct {
-	body string
-	urls []string
-}
-
-func (f fakeFetcher) Fetch(url string) (string, []string, error) {
-	if res, ok := f[url]; ok {
-		return res.body, res.urls, nil
+	
+	puzzle := [9][9]int{	{3,0,0,9,6,0,0,0,0},
+							{1,4,0,0,0,5,0,9,0},
+							{0,0,5,0,0,0,0,0,8},
+							{0,0,0,0,5,0,0,2,0},
+							{0,0,3,8,0,0,0,1,9},
+							{0,0,0,6,4,0,0,3,0},
+							{0,0,0,0,0,0,0,0,1},
+							{8,0,0,0,2,0,0,0,0},
+							{0,0,1,0,0,3,0,0,4},
 	}
-	return "", nil, fmt.Errorf("not found: %s", url)
-}
-
-// fetcher is a populated fakeFetcher.
-var fetcher = fakeFetcher{
-	"http://golang.org/": &fakeResult{
-		"The Go Programming Language",
-		[]string{
-			"http://golang.org/pkg/",
-			"http://golang.org/cmd/",
-		},
-	},
-	"http://golang.org/pkg/": &fakeResult{
-		"Packages",
-		[]string{
-			"http://golang.org/",
-			"http://golang.org/cmd/",
-			"http://golang.org/pkg/fmt/",
-			"http://golang.org/pkg/os/",
-		},
-	},
-	"http://golang.org/pkg/fmt/": &fakeResult{
-		"Package fmt",
-		[]string{
-			"http://golang.org/",
-			"http://golang.org/pkg/",
-		},
-	},
-	"http://golang.org/pkg/os/": &fakeResult{
-		"Package os",
-		[]string{
-			"http://golang.org/",
-			"http://golang.org/pkg/",
-		},
-	},
+	
+	var g grid
+	
+	g.Fill(puzzle)
+	fmt.Println(g)
 }
 
